20cd6b69509b7a9900c32a8b3b7c806bb0656c63
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-19 14:01:28 +0530

    uncomment

--- backend/src/main/java/com/example/backend/entity/Alert.java
+++ backend/src/main/java/com/example/backend/entity/Alert.java
@@ -1,19 +1,19 @@
-//package com.example.backend.entity;
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Table(name = "alerts")
-//public class Alert {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String type;
-//    private String message;
-//}
+package com.example.backend.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "alerts")
+public class Alert {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+    private String message;
+}
--- backend/src/main/java/com/example/backend/entity/GarbageTruck.java
+++ backend/src/main/java/com/example/backend/entity/GarbageTruck.java
@@ -1,19 +1,19 @@
-//package com.example.backend.entity;
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Table(name = "garbage_trucks")
-//public class GarbageTruck {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String location;
-//    private Boolean availability;
-//
-//}
+package com.example.backend.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "garbage_trucks")
+public class GarbageTruck {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String location;
+    private Boolean availability;
+
+}
